4916111bb3d5b761dcfcec76f98084fe52a7baf3
Victor Buendia <victorbuendia03@gmail.com>
2025-03-29 19:57:29 -0300

    💚 Fix CI

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoPath = 'aws-icon-finder';
+
 const nextConfig: NextConfig = {
     output: 'export',
+    assetPrefix: `/${repoPath}/`,
+    basePath: `/${repoPath}`,
     images: {
         unoptimized: true,
     },
